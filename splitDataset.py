fea3e53df0a1cc30b022b5b9e5d89ea6ec54f9c2
Sudharshan <72936645+sudharshan2001@users.noreply.github.com>
2022-08-11 19:38:01 +0530

    Update splitDataset.py

--- splitDataset.py
+++ splitDataset.py
@@ -3,8 +3,8 @@ def split_data (df, split_prop=[0.8,0.2]):
     zeros = df.filter(df["label"]==0)
     ones = df.filter(df["label"]==1)
 
-    train0, test0 = zeros.randomSplit(SPLIT_VALS, seed=1234)
-    train1, test1 = ones.randomSplit(SPLIT_VALS, seed=1234)
+    train0, test0 = zeros.randomSplit(split_prop, seed=69)
+    train1, test1 = ones.randomSplit(split_prop, seed=69)
 
     train_set = train0.union(train1)
 
